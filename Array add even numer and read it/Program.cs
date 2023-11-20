3bf354090a0a873b035eb0ca1973593754b6e843
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-20 21:55:52 +0530

    Add files via upload

---
+++ Array add even numer and read it/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_add_even_numer_and_read_it
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Eneter the target number ");
+            int target = int.Parse(Console.ReadLine());
+
+            int evencount = target / 2;
+            int[] evens = new int[evencount];
+            int counter = 2;
+                
+            for(int i = 0; i < evencount; i++ , counter+=2)
+            {
+                evens[i] = counter;
+            }
+            for (int i = 0; i < evens.Length; i++)
+            {
+                Console.Write($"{evens[i]} ");
+            }]]]]]]]]]]f
+            Console.WriteLine();
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

7d7a76779407c4228b89e57fe629fae0855deb85
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-20 09:53:49 +0530

    Add files via upload

---
+++ find target even count/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methods_in
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the target number ; ");
+            int target = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < target; i++)
+            {
+                if (i % 2 == 0)
+                    Console.Write($"{i} ");
+            }
+            Console.WriteLine(); // new line 
+
+
+            Console.ReadLine();
+        }
+    }
+}

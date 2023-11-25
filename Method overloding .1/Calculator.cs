336ab1bbcd673b59b2565451b5fc113eba7f4d08
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-25 12:51:24 +0530

    Add files via upload

---
+++ Method overloding .1/Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_overloding._1
+{
+    public class Calculator
+    {
+        public void print() // Method  
+        {
+            Console.WriteLine("print() called ");
+        }
+
+        public void print(int num) // Method  
+        {
+            Console.WriteLine("print(10) called ");
+        }
+
+        public void print(ref int num) // Method  
+        {
+            Console.WriteLine("print(ref int num) called ");
+        }
+
+        public void print(int num1, int num2) // Method  
+        {
+            Console.WriteLine("print(int num1, int num2)) called ");
+        }
+
+        public void print(string name ) // Method  
+        {
+            Console.WriteLine("print( string name ) called ");
+        }
+
+        public void print(int num3 ,string name) // Method  
+        {
+            Console.WriteLine("print( int num3 ,string name ) called ");
+        }
+
+        public void print( string name1, int num4) // Method  
+        {
+            Console.WriteLine("print( string name1, int num4e ) called ");
+        }
+
+    }
+}
---
+++ Method overloding .1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_overloding._1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator c1 = new Calculator(); //object of the class 
+            c1.print(); //called method 
+                        //we can pass the argument over here    
+
+            c1.print(10);
+
+            int i = 1000;
+            c1.print(ref i );
+
+            c1.print(10, 20);
+
+            c1.print("radhesh");
+
+            c1.print(1,"sham");
+
+            c1.print("prathm", 1);
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

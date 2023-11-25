bda453d80c644740ec682234def1742d220afd5a
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-25 11:43:19 +0530

    Add files via upload

---
+++ Polymorphism/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class Program
+    {
+       public void sum( int a ,int b )    
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void sum(float a , float b)
+        {
+            Console.WriteLine(a+b);
+        } 
+
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.sum(10, 20);
+
+
+
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}

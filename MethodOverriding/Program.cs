75e92c8d5638d726eec8f8c70583d15411aea9a9
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-02 18:17:29 +0530

    Add files via upload

---
+++ MethodOverriding/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverriding
+{
+    public class a
+    {
+        public virtual void M1() //method
+        {
+            Console.WriteLine("Method of base class "); //method body
+        }
+    }
+    public class b : a 
+    {
+        public override void M1()
+        {
+            Console.WriteLine("Method of derived // child  class ");
+        }
+
+
+    }
+
+    public class c
+    {
+        public static void Main()
+        {
+            b b1 = new b();// object 
+            b1.M1();// calling the obj
+            Console.ReadLine();
+        }
+    }
+
+}

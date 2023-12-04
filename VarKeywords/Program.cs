6cc82b39f327691a0cd4634b7f1995b6fdc34d9a
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-04 18:38:00 +0530

    Add files via upload

--- VarKeywords/Program.cs
+++ VarKeywords/Program.cs
@@ -10,25 +10,8 @@ namespace VarKeywords
     {
         static void Main(string[] args)
         {
-            var n = "abhishek";
-            Console.WriteLine(n);
-            Console.WriteLine(n.GetType());
-
-            var s = 100;
-            Console.WriteLine(s);
-            Console.WriteLine(s.GetType());
-
-            var a = 10f;
-            Console.WriteLine(a);
-            Console.WriteLine(a.GetType());
-
-            var b = 455m;
-            Console.WriteLine(b);
-            Console.WriteLine(b.GetType());
-
-            var d = true;
-            Console.WriteLine(d);
-            Console.WriteLine(d.GetType());
+            Console.WriteLine("sorry aaj mood nahi he ");
+            
 
 
             

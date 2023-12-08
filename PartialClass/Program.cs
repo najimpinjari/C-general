80c36362fe2689d87412b90d37d5ffe4bb023bc7
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-08 18:18:13 +0530

    Add files via upload

---
+++ PartialClass/Program.cs
@@ -0,0 +1,89 @@
+﻿
+using System;
+
+namespace PartialClass
+{
+    public partial class Employee
+    {
+        public void EmployeeName()
+        {
+            Console.WriteLine("Emp name Rupesh Raut ");
+        }
+    }
+
+    public partial class Employee
+    {
+        public void EmployeeDep()
+        {
+            Console.WriteLine("IT department ");
+        }
+    }
+
+    public partial class Employee
+    {
+        public void EmployeeSalary()
+        {
+            Console.WriteLine("100000 ");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.EmployeeName();
+            emp.EmployeeDep();
+            emp.EmployeeSalary();
+        }
+    }
+}
+
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace PartialClass
+//{
+//    public partial class Employee
+//    {
+//        public void EmployeeName()
+//        {
+//            Console.WriteLine("Emp name Rupesh Raut ");
+//        }
+
+//    }
+//    public partial class Employee
+//    {
+//        public void EmployeeDep()
+//        {
+//            Console.WriteLine("IT department ");
+
+
+//        }
+
+//    }
+//    public partial class Employee
+//    {
+//        public void EmployeeSalary()
+//        {
+//            Console.WriteLine("100000 ");
+
+//        }
+//    }
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Employee emp = new Employee();
+//            emp.EmployeeName();
+//            emp.EmployeeDep();
+//            emp.EmployeeSalary();
+
+
+//        }
+//    }
+//}
---
+++ PartialClass/ProgramBase.cs
@@ -0,0 +1,13 @@
+﻿namespace PartialClass
+{
+    internal class ProgramBase
+    {
+        static void Main(string[] )
+        {
+            Employee emp = new Employee();
+            emp.EmployeeName();
+            emp.EmployeeDep();
+            emp.EmployeeSalary();
+        }
+    }
+}

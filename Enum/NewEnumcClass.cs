3a293b8c01335e891880d73434b64327aa1aeb40
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-11 22:51:07 +0530

    Add files via upload

---
+++ Enum/NewEnumcClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public enum NewEnumcClass //syntax of enum 
+    {
+        monday,
+        tuesday,
+        wednesday,
+        thursday,
+        friday,
+        saturday,
+        sunday
+    }
+}
---
+++ Enum/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //for string output 
+
+            Console.WriteLine("enum name " + NewEnumcClass.thursday);
+
+            //for integer output 
+            Console.WriteLine("enum int output : " + (int)NewEnumcClass.friday);
+            Console.ReadLine();
+        }
+    }
+}

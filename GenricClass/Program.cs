7828a8355778588b73caa9aa4dbefde715fa72fc
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-11 18:49:23 +0530

    Add files via upload

---
+++ GenricClass/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricClass
+{   public class MainClass
+    { 
+        private static void Main()
+        {
+            bool Equal = Calculater.AreEqual<string>("aaaa", "aaa");
+
+            if( Equal )
+            {
+                Console.WriteLine("Equal");
+            }
+            else 
+            {
+                Console.WriteLine("Not Equal");
+
+                Console.ReadLine();
+
+            }
+
+        }
+
+    }
+    public  class Calculater
+    {
+        public static bool AreEqual<T>(T value1 , T value2)
+        {
+            return value1.Equals(value2 );
+
+        }
+
+    }
+
+}

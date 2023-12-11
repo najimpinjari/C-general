566af6bac43a8e41b1fc9aeda7708979b6a31e3f
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-11 18:50:16 +0530

    Add files via upload

---
+++ GenricsOne/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricsOne
+{
+    public class Calculator
+    {
+        #region method overloding 
+        //public bool AreEqual(int a , int b)
+        //{
+        //    return a.Equals(b); 
+        //}
+        //public bool AreEqual(string a, string b)
+        //{
+        //    return a.Equals(b);
+        //}
+
+        #endregion method overloding 
+
+        #region using object type 
+
+        //public bool AreEqual( object a , object b)
+        //{
+        //    return a.Equals(b);
+        //}
+        //problem with object typs 
+        //1 there is no type sefty 
+        //2 chances of boxing and unboxing operations 
+        #endregion using object type 
+
+        #region using generics 
+
+        public bool AreEqual<T>(T a, T b)
+        {
+            return a.Equals(b);
+        }
+
+
+
+        #endregion using generics 
+
+    }
+}
---
+++ GenricsOne/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenricsOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calculator c1 = new Calculator();
+            //bool b1 = c1.AreEqual(10, 10);
+            //Console.WriteLine(b1); //true
+
+            //b1 = c1.AreEqual(10, 11);
+            //Console.WriteLine(b1); // false 
+
+            //b1 = c1.AreEqual("NAZ", "NAJ");
+            //Console.WriteLine(b1); // false 
+
+            Calculator c1 = new Calculator();
+            bool b1 = c1.AreEqual<int>(10, 10);
+            Console.WriteLine(b1);//true
+
+            b1 = c1.AreEqual<string>("a", "v");
+            Console.WriteLine(b1); // false
+
+            b1 = c1.AreEqual<float>(10.3f, 4.2f);
+            Console.WriteLine(b1); //false
+
+            Console.ReadLine();
+        }
+    }
+}

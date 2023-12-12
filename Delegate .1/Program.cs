9dfce496f94c60cc9f9309e6caeaeece94ada1a9
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-12 22:11:56 +0530

    Add files via upload

---
+++ Delegate .1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate._1
+{   delegate void calculator(int a , int b);
+    class Program
+    {   public static void Add(int a ,  int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        public static void Mul(int a, int b)
+        {
+            Console.WriteLine(a * b);
+        }
+        static void Main(string[] args)
+        {
+            calculator clc = new calculator(Add);
+            clc += Mul; //thats called malticast delegates 
+            //delegated pointing more than one methods 
+            clc(5, 20);
+
+            //calculator clc1 = new calculator(Mul);
+            //clc1(20, 20);
+
+            Console.ReadLine();
+        }
+    }
+}

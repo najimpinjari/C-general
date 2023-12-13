2ef32c56ee4fa15a990c270b7b8f79b1beffc1b4
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-13 23:31:54 +0530

    Add files via upload

---
+++ stringbuilder/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stringbuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+
+            sb.AppendLine("najim");
+
+            sb.AppendLine("pinjari");
+
+            string result = sb.ToString();
+            Console.WriteLine(result); 
+
+            Console.ReadLine();
+
+        }
+    }
+}

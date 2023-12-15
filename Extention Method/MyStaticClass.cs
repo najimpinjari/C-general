afa6fb9bfc9f467548030a2927b3cbef4a79e89c
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-15 17:13:04 +0530

    Add files via upload

---
+++ Extention Method/MyStaticClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extention_Method
+{
+    static class MyStaticClass
+    {
+        public static void Funk3(this Program pm)
+        {
+            Console.WriteLine("this is third program ");
+        }
+
+    }
+
+}
---
+++ Extention Method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extention_Method
+{
+    class Program
+    {   
+        public void Funk1()
+        {
+            Console.WriteLine("this is first function ");
+        }
+
+        public void Funk2()
+        {
+            Console.WriteLine("this is second function ");
+        }
+
+        static void Main(string[] args)
+        {
+            Program pm = new Program();
+            pm.Funk1();
+            pm.Funk2();
+            pm.Funk3();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

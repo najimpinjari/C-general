1f2c3abf77105abd5647a5544e0237eff8f7f09c
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-16 18:00:59 +0530

    Add files via upload

---
+++ Collection_Class_Hashtable/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Class_Hashtable
+{
+    class Program
+    {
+        public static object Keys { get; private set; }
+
+        static void Main(string[] args)
+        {
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "najim");
+            ht.Add(2, "sham");
+            ht.Add(3, "gotya");
+
+
+            Console.WriteLine(ht.Count);
+            Console.WriteLine(ht.Keys);
+            Console.WriteLine(ht.Values);
+
+
+            //Console.WriteLine(ht.Count("najim"));
+            //Console.WriteLine(ht.Contains(2));
+            //Console.WriteLine(ht.ContainsValue("najim")); //for finding keys or vlues k
+
+
+            //Console.WriteLine(ht[3]); //call or display the 3rd values using key 
+
+            //foreach (object key in  ht.Keys) 
+            //{
+            //    Console.WriteLine( key + "  : " +ht[key]);
+            //}
+
+
+            ////ht.Clear();
+            //////ht.Remove(1);
+            //Console.WriteLine("------------");
+
+
+            //foreach (object key in ht.Keys)
+            //{
+            //    Console.WriteLine(key + "  : " + ht[key]);
+            //}
+
+
+
+            //foreach (var item in ht)
+            //{
+            //    Console.Write(ht[1]);
+            //}
+            //Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
+

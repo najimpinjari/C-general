e25e7bdb3c4b8dfe911c33133f976fbd433a7a11
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-16 18:01:52 +0530

    collection types

---
+++ Collection_stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Collection_stack
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {   
+            Stack<int> newstack = new Stack<int>();
+            {
+                newstack.Push(1);
+                newstack.Push(2);
+                newstack.Push(3);
+                newstack.Push(4);
+                newstack.Push(5);
+                newstack.Push(6);
+            }
+
+
+            foreach (int i in newstack)
+            {
+                Console.WriteLine(i);
+            }
+            newstack.Pop();
+            Console.WriteLine("afer pop of the element");
+
+            foreach (int j in newstack)
+            {
+                Console.WriteLine(j);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

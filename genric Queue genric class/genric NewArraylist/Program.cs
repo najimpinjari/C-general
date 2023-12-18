2e591a33ccdfacecc3fd85c20ee9b1613626dd53
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-18 18:16:35 +0530

    Add files via upload

---
+++ genric Queue genric class/genric NewArraylist/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraylistNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Array
+            ////make a array 
+
+            //int[] numbers = new int[3];
+            //numbers[0] = 1;
+            //numbers[1] = 2;
+            //numbers[2] = 3;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            #endregion Array
+
+            //Make a Arraylist
+
+            //Console.BackgroundColor = ConsoleColor.Magenta;
+            //Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+            ArrayList l1 = new ArrayList();
+            l1.Add(10);
+            l1.Add(20);
+            l1.Add(30);
+            l1.Add(40);
+            l1.Add(50);
+            l1.Add(60);
+            l1.Add(70);
+
+
+            for(int i = 0; i < l1.Count; i++) 
+            {
+                Console.WriteLine(l1[i]);
+            }
+            foreach (int i in l1)
+            {
+                Console.Write(i);
+            }
+
+            /*ICollection collection = new ArrayList() *//*{ 11, 12, 13 };*/
+            //l1.AddRange(l1 );
+
+            l1.Insert(11, 12, 13, 14);
+            
+
+            Console.WriteLine("-------------");
+
+
+            for (int i = 0; i < l1.Count; i++)
+            {
+                Console.WriteLine(l1[i]);
+            }
+
+
+            /*  ArrayList numbers = new ArrayList(4);
+              numbers.Add(2);
+              numbers.Add(3);
+              numbers.Add(4);
+              numbers.Add(10);
+
+              Console.WriteLine("original collection");
+
+              for (int i = 0; i < numbers.Count; i++)
+              {
+                  Console.Write($"{numbers[i]}  ");
+              }
+              Console.WriteLine();
+
+              //foreach (int i in numbers)
+              //{   
+              //    Console.Write(numbers[i]);
+              //}
+
+              //Add multiple items
+
+              ICollection coll = new ArrayList() { 40, 41, 42, 43, 44 };
+              numbers.AddRange(coll);
+
+              Console.WriteLine(" addrange(call) callection ");
+
+              for (int i = 0; i < numbers.Count; ++i)
+              {
+                  Console.Write($" {numbers[i]}  ");
+              }
+              Console.WriteLine();
+              //2   3   4   10   40   41   42   43   44
+
+              //numbers.Add(19);  
+              numbers.Insert(1, 1);
+
+              Console.WriteLine(" add( 1) ");
+
+              for(int i = 0; i < numbers.Count; i++)
+              {
+                  Console.Write( $" { numbers[i]} "  );
+              }
+              Console.WriteLine();
+
+              ICollection call = new ArrayList() { 11, 12, 13, 14, 15, 16, 17, 18, };
+              numbers.InsertRange(0, call);
+
+              Console.WriteLine(" add( { 11, 12, 13, 14, 15, 16, 17, 18, };) ");
+
+              for (int i = 0; i < numbers.Count; i++)
+              {
+                  Console.Write($" {numbers[i]} ");
+              }
+              Console.WriteLine();
+
+             bool b1 =  numbers.Contains(11);
+              Console.WriteLine(b1);
+
+              bool b2 = numbers.Contains(99);
+              Console.WriteLine(b2);
+
+              numbers.Remove(11);
+              numbers.RemoveAt(0);
+
+              Console.WriteLine(" add(  numbers.Remove(11);   numbers.RemoveAt(0); ");
+
+              for (int i = 0; i < numbers.Count; i++)
+              {
+                  Console.Write($" {numbers[i]} ");
+              }
+
+
+  */
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}

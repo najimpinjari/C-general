86aeb8223d443873fbea86268802ce72e1e5cd9c
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-19 18:20:13 +0530

    Add files via upload

---
+++ tuplesDemo/tuple class 1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tuple_class_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            //creating a tuple with two elements int and sring 
+
+            Tuple<int, string> mytuple = new Tuple<int, string>(42, "hello world");
+            
+
+            //accesing the item of the tuple 
+            int intvalue = mytuple.Item1;
+            string stringvalues = mytuple.Item2;
+
+            //disply a values 
+            Console.WriteLine(stringvalues);
+            Console.WriteLine(intvalue);
+
+            //createing a tuple using literes 
+            var anothertuple = (10, "c# tuple");
+
+            var (number , message ) = anothertuple;
+
+            Console.WriteLine($" numbers {number}");
+            Console.WriteLine($" message  {message}");
+            Console.ReadLine();
+
+        }
+    }
+}

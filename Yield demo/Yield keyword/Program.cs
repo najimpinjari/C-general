ef22d07d382fc4af0a0db1829a44ed20de01679b
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-20 18:30:50 +0530

    Add files via upload

---
+++ Yield demo/Yield keyword/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yield_keyword
+{
+    class Program //yeald keyword 
+    {   
+        static IEnumerable<int> yeild1()
+        {
+            yield return 1;
+            yield return 2;
+            yield return 3;
+        }
+
+
+        static void Main(string[] args)
+        {
+            foreach (int i in yeild1())
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

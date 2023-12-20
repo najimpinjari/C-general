f68650a777a25b4c53c567a1f775cae3ab6f7705
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-20 18:33:28 +0530

    Add files via upload

---
+++ recursion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace recursion
+{
+    class Program
+    {
+        int num = 1; // Recursion is the proces by virtual of which of method can call itself 
+        void counting()
+        {
+            Console.WriteLine(num);
+            num++;
+            counting();
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.counting();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}

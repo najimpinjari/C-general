ea57c843dece52e6e0d3f6316bd6dee36ad512dd
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-21 12:45:05 +0530

    Add files via upload

---
+++ lambda expression/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda_expression
+{
+    public delegate void Mydelegate(int num); //delegate we use another function pointing   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mydelegate obj = /*delegate*/ (/*int*/ a) =>  //anonynimus function , look the (int a )( int num) my delegate and there objects signature should 
+            //same at every time to do   
+                                               //the above arrow is lambda expression 
+            {
+                a += 5;
+                Console.WriteLine(a);
+            };
+
+            obj.Invoke(10); //for callind method 
+            Console.ReadLine(); //for holding console windo
+        }
+    }
+}

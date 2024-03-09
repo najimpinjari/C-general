5680e2a24dbf9677bdb22cf5c851456dd9cd4901
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-03-09 19:07:45 +0530

    base

---
+++ fab_9/fab_9/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace fab_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring variables of different types
+            //int num = 10;
+            //string text = "Hello";
+            //double dbl = 3.14;
+            //DateTime date = DateTime.Now;
+
+            //// Implicitly converting variables to object type
+            //object obj1 = num;
+            //object obj2 = text;
+            //object obj3 = dbl;
+            //object obj4 = date;
+
+            //// Printing the variables and their types
+            //Console.WriteLine($"obj1: {obj1}, Type: {obj1.GetType()}");
+            //Console.WriteLine($"obj2: {obj2}, Type: {obj2.GetType()}");
+            //Console.WriteLine($"obj3: {obj3}, Type: {obj3.GetType()}");
+            // Console.WriteLine($"obj4: {obj4}, Type: {obj4.GetType()}");
+
+            // var keyword
+
+            //string name = "rupesh";
+
+            //var name1 = "najim";
+
+            //Console.WriteLine(name1.GetType());
+            //Console.WriteLine(name.GetType());
+
+            //var person = { name = "rupesh", Age = 23 };
+
+            //Console.WriteLine(person);
+
+            //anannimas class types
+
+
+            //var person = new { Name = "John", Age = 30 };
+
+            //Console.WriteLine( $"{person.Age} ,{person.Name}");
+
+            //
+
+        }
+    }
+}

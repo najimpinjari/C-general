7854a6556a84f6ae127239a698aaf7739f27d414
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-05-02 18:49:21 +0530

    Add files via upload

---
+++ Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a new instance of the Child class
+            Child child = new Child();
+
+            // Call methods from both Parent and Child classes
+            child.DisplayParentMessage();
+            child.DisplayChildMessage();
+        }
+    }
+
+    // Parent class
+    class Parent
+    {
+        public void DisplayParentMessage()
+        {
+            Console.WriteLine("This is a message from the Parent class.");
+        }
+    }
+
+    // Child class inherits from Parent class
+    class Child : Parent
+    {
+        public void DisplayChildMessage()
+        {
+            Console.WriteLine("This is a message from the Child class.");
+        }
+    }
+
+}
